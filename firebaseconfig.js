d98a2fad638f8e513b01a5b6447bc94416197270
Aryan Maurya <121174931+aryan9967@users.noreply.github.com>
2023-12-16 23:12:52 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAkhsZFQusqJeZLJOWG2uq8kK444ZaqXfI",
-    authDomain: "aryan1-e9542.firebaseapp.com",
-    projectId: "aryan1-e9542",
-    storageBucket: "aryan1-e9542.appspot.com",
-    messagingSenderId: "10059339852",
-    appId: "1:10059339852:web:0ac762b4de4bfd5bcd3ce9",
-    measurementId: "G-PXQ1M737D3"
+    apiKey: //api key,
+    authDomain: //auth domain,
+    projectId: //project id,
+    storageBucket: //storage bucket name,
+    messagingSenderId: // sender ID,
+    appId: // app ID,
+    measurementId: //measurement ID
   };
